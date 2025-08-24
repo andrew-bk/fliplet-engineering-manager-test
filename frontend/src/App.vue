a2bd4809dc512115ee4886486d2944d281746ab1
andrew-growthintel <andrew@adzact.com>
2025-08-24 14:11:33 +0100

    Created project wrapper for drag-drop to test assumption on as-is code. Assumptino is that dragging and dropping Text and Button will add the text "Text" and "Button" to the preview section

---
+++ frontend/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <header>
+      <h1>Vue Drag & Drop Application</h1>
+    </header>
+    <main>
+      <DragDrop />
+    </main>
+  </div>
+</template>
+
+<script>
+import DragDrop from './components/drag-drop.vue'
+
+export default {
+  name: 'App',
+  components: {
+    DragDrop
+  }
+}
+</script>
+
--- frontend/src/components/drag-drop.vue
+++ frontend/src/components/drag-drop.vue
@@ -13,6 +13,7 @@
 
 <script>
 export default {
+  name: "DragDrop",
   data() {
     return { items: [] };
   },
